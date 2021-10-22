37fd30071bc1c494d04c36310ab2feaec64f1284
devHikary <wrkkarina@gmail.com>
2021-10-22 07:54:40 -0300

    Assets e Header

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { Home } from './src/screens/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
 
 
 export default function App() {
-  return (
-    <View >
-      <Home />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+  return <Home />;
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 import { View, Text} from 'react-native'
+import { Header } from '../../components/Header';
 import { styles } from './styles';
 
 export function Home () {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Componente home</Text>
 
     </View>
