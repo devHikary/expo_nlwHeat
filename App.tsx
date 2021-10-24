378eca253dc51cb4d622cb79e5d70e141e1531e9
devHikary <wrkkarina@gmail.com>
2021-10-24 12:19:22 -0300

    hook

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { Home } from './src/screens/Home';
+import React from "react";
+import { Home } from "./src/screens/Home";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
-
+import { StatusBar } from "expo-status-bar";
+import { AuthProvider } from "./src/hooks/auth";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold,
   });
 
-  if(!fontsLoaded) {
-    return <AppLoading/>
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-  return <Home />;
+  return (
+    <AuthProvider>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <Home />
+    </AuthProvider>
+  );
 }
